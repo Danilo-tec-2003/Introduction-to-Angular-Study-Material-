fb6ca52ae0e9b08b4fe876689540470f903d69eb
Danilo Mendes <danilodev.br@gmail.com>
2025-03-18 23:30:03 -0300

    inicio da estrutura

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+{
+    path: "",
+    component: HomeComponent
+},
+{
+    path: "home",
+    component: HomeComponent
+},
+
+];
